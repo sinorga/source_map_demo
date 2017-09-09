33db16190247f0acbda61e540e9fa9df2ff71008
orga <orga.shih@gmail.com>
2017-09-09 17:35:34 +0800

    add uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const webpack = require('webpack');
 const config = {
   entry: './src/index.js',
   output: {
@@ -7,6 +7,9 @@ const config = {
     filename: 'bundle.js'
   },
   devtool: 'source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({sourceMap: true}),
+  ],
 };
 
 module.exports = config;
